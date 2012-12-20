76e90dbc337c2fec448d5ed5fb5b87216ef1d7bc
chessprojet <chessprojet@gmail.com>
2012-12-20 12:07:00 +0100

    Commit du Fou.java

--- projet/src/main/java/projet/classes/Fou.java
+++ projet/src/main/java/projet/classes/Fou.java
@@ -41,7 +41,7 @@ public class Fou extends Piece {
 		// Teste tous les déplacements possibles dans la diagonale descendante droite pour ne pas sortir de l'échiquier
 		for (int pos=1; pos<8; pos++) {
 			// N'ajoute que les déplacements qui ne sortent pas de l'échiquier dans la liste
-			if (w+9*pos%8==7 && mdroite==true) {
+			if ((w+9*pos)%8==7 && mdroite==true) {
 				mdroite=false; this.deplacement.add(9*pos); this.nbsoldir.set(0,this.nbsoldir.get(0)+1);
 			}
 			if (mdroite==true) {
@@ -52,7 +52,7 @@ public class Fou extends Piece {
 		// Teste tous les déplacements possibles dans la diagonale descendante gauche pour ne pas sortir de l'échiquier
 		for (int pos=1; pos<8; pos++) {
 			// N'ajoute que les déplacements qui ne sortent pas de l'échiquier dans la liste
-			if (w+7*pos%8==0 && mgauche==true) {
+			if ((w+7*pos)%8==0 && mgauche==true) {
 				mgauche=false;this.deplacement.add(7*pos);this.nbsoldir.set(1,this.nbsoldir.get(1)+1);
 			}
 			if (mgauche==true) {
@@ -63,7 +63,7 @@ public class Fou extends Piece {
 		// Teste tous les déplacements possibles dans la diagonale montante droite pour ne pas sortir de l'échiquier
 		for (int pos=1;pos<8;pos++) {
 			// N'ajoute que les déplacements qui ne sortent pas de l'échiquier dans la liste
-			if (w-7*pos%8==7 && ddroite==true) {
+			if ((w-7*pos)%8==7 && ddroite==true) {
 				ddroite=false;this.deplacement.add(-7*pos); this.nbsoldir.set(2,this.nbsoldir.get(2)+1);
 			}
 			if (ddroite==true) {
@@ -74,7 +74,7 @@ public class Fou extends Piece {
 		// Teste tous les déplacements possibles dans la diagonale montante gauche pour ne pas sortir de l'échiquier
 		for (int pos=1;pos<8;pos++) {
 			// N'ajoute que les déplacements qui ne sortent pas de l'échiquier dans la liste
-			if (w-9*pos%8==0 && dgauche==true) {
+			if ((w-9*pos)%8==0 && dgauche==true) {
 				dgauche=false;this.deplacement.add(-9*pos); this.nbsoldir.set(3,this.nbsoldir.get(3)+1);
 			}
 			if (dgauche==true) {
